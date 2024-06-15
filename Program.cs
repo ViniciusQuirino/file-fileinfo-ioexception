cfb2585c5e007b6642d58392b567d3fa0773e445
ViniciusQuirino <viniciusquirino13@hotmail.com>
2024-06-15 14:15:51 -0300

    fileInfo

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = "/home/vinicius/Área de Trabalho/file-fileinfo-ioexception/file1.txt";
+            string targetPath = "/home/vinicius/Área de Trabalho/file-fileinfo-ioexception/file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
